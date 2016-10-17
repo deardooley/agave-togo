5484ff92d6cdeffd9e640d6c362aa4e35fc7e6e4
mrojas <mrojas@wireless-10-147-210-114.public.utexas.edu>
2016-10-17 11:51:00 -0500

    Added file permission messages for filemaanger

--- app/js/main.js
+++ app/js/main.js
@@ -179,6 +179,8 @@ AgaveToGo.config(function($translateProvider) {
 
     success_apps_permissions_update: 'Success: updated permissions for ',
 
+    success_files_permissions_update: 'Success updating file permissions',
+
     success_monitors_test: 'Success: fired monitor ',
     success_monitors_update: 'Success: updated ',
 
