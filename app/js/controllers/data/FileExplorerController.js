eb47214d9aa54048911f4667dc19372c6d12cd31
Rion Dooley <dooley@tacc.utexas.edu>
2017-04-26 09:58:33 -0500

    * Cleaning up the file manager dependencies lazy loaded on job submission pages.

--- app/js/controllers/data/FileExplorerController.js
+++ app/js/controllers/data/FileExplorerController.js
@@ -165,19 +165,6 @@ angular.module('AgaveToGo').controller('FileExplorerController', function($rootS
     }
 
     $scope.lazyLoadFileManagerParams = [
-      // '../bower_components/angular-filebrowser/src/js/app.js',
-      // '../bower_components/angular-filebrowser/src/js/providers/config.js',
-      // '../bower_components/angular-filebrowser/src/js/directives/directives.js',
-      // '../bower_components/angular-filebrowser/src/js/filters/filters.js',
-      // '../bower_components/angular-filebrowser/src/js/entities/acl.js',
-      // '../bower_components/angular-filebrowser/src/js/entities/chmod.js',
-      // '../bower_components/angular-filebrowser/src/js/entities/fileitem.js',
-      // '../bower_components/angular-filebrowser/src/js/entities/item.js',
-      // '../bower_components/angular-filebrowser/src/js/services/filenavigator.js',
-      // '../bower_components/angular-filebrowser/src/js/services/fileuploader.js',
-      // '../bower_components/angular-filebrowser/src/js/providers/translations.js',
-      // '../bower_components/angular-filebrowser/src/js/controllers/main.js',
-      // '../bower_components/angular-filebrowser/src/js/controllers/selector-controller.js',
       '../bower_components/angular-cookies/angular-cookies.min.js',
       '../bower_components/angular-filebrowser/dist/agave-angular-filemanager.min.js',
       '../bower_components/angular-filebrowser/dist/agave-angular-filemanager.min.css',
