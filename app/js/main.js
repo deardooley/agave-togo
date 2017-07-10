704e3f987447a5ea2ebe460013e16275d047234e
Rion Dooley <dooley@tacc.utexas.edu>
2017-07-10 11:25:05 -0500

    *missing comma broke syntax on run method.

--- app/js/main.js
+++ app/js/main.js
@@ -2281,7 +2281,7 @@ AgaveToGo.config(['$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryPro
 
 /* Init global settings and run the app */
 //AgaveToGo.run(['$rootScope', 'settings', '$state', 'ProfilesController', function($rootScope, settings, $state) { //}, ProfilesController) {
-AgaveToGo.run(['$rootScope', 'settings', '$state', '$http', '$templateCache', '$localStorage', '$window', 'CacheFactory', 'TokensController', 'userProperties', 'ProfilesController'
+AgaveToGo.run(['$rootScope', 'settings', '$state', '$http', '$templateCache', '$localStorage', '$window', 'CacheFactory', 'TokensController', 'userProperties', 'ProfilesController',
   function ($rootScope, settings, $state, $http, $templateCache, $localStorage, $window, CacheFactory, TokensController, userProperties, ProfilesController) {
     $rootScope.$state = $state; // state to be accessed from view
     $rootScope.$settings = settings; // state to be accessed from view
